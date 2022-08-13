50d7221be2d17465650aa44a7b4583c251a0835b
Son Tran <son.tran@team.enouvo.com>
2022-08-13 17:04:47 +0700

    Init project

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { domain } from 'lib/config'
+import { resolveNotionPage } from 'lib/resolve-notion-page'
+import { NotionPage } from 'components'
+
+export const getStaticProps = async () => {
+  try {
+    const props = await resolveNotionPage(domain)
+    return { props, revalidate: 10 }
+  } catch (err) {
+    console.error('page error', domain, err)
+    throw err
+  }
+}
+
+export default function NotionDomainPage(props) {
+  console.log(props)
+  return <NotionPage {...props} />
+}
